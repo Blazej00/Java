c3ea19d97a14f263d8898427b056144b46932832
Blazej00 <145206254+Blazej00@users.noreply.github.com>
2024-10-15 20:53:19 +0200

    Update zad2_obr_zewnetrzmn.js

--- zad2_obr_zewnetrzmn.js
+++ zad2_obr_zewnetrzmn.js
@@ -1,2 +1,2 @@
- document.write("<u>"+"ostatnia modyfikacja strony".fontcolor("#60000").bold().fontsize(6)+"</u>"+"<br>");
+ document.write("<u>"+"ostatnia modyfikacja strony".fontcolor("#60000").fontsize(6)+"</u>"+"<br>");
 document.write(document.lastModified);
